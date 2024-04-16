e90587120358b98821c73b9cdb5bd860cfc6a416
vivek <19singhvivek@gmail.com>
2024-04-16 10:35:47 +0530

    this is react app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Dashboard from './components/dashboard';
-import {Header} from './components/header';
+import Header from './components/header';
 import Node from './components/Node';
 import Nesting from './components/nesting';
 
